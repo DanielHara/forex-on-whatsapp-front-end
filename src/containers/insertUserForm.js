17f144868ab890dcbf4bd6f89467153deb36e5d6
Daniel Hara <haradaniel734@gmail.com>
2018-12-05 15:17:37 -0200

    allows multiple currencies

--- src/containers/insertUserForm.js
+++ src/containers/insertUserForm.js
@@ -3,6 +3,11 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  padding: 5px;
+`;
 
 
 const currencies = ['BTC', 'USD', 'CHF', 'GBP'];
@@ -10,23 +15,42 @@ const currencies = ['BTC', 'USD', 'CHF', 'GBP'];
 class InsertUserForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      name: "",
+      phoneNumber: "",
+      selectedCurrencies: [currencies[0]],
+    }
   }
 
   handleNameChange = (e) => {
-    this.setState({ 'name': e.target.value});
+    this.setState({ name: e.target.value});
   };
 
   handlePhoneNumberChange = (e) => {
-    this.setState({ 'phoneNumber': e.target.value });
+    this.setState({ phoneNumber: e.target.value });
   };
 
-  handleCurrencyChange = (e) => {
-    this.setState({ 'selectedCurrency': e.target.value });
+  handleCurrencyChange = (e, index) => {
+    const newSelectedCurrencies = this.state.selectedCurrencies.slice();
+    newSelectedCurrencies[index] = e.target.value;
+    this.setState({
+      selectedCurrencies: newSelectedCurrencies,
+    });
+  };
+
+  handleCurrencyAdded = (e) => {
+    if (this.state.selectedCurrencies.length >= currencies.length) {
+      alert(`You can choose between at most ${currencies.length} currencies.`);
+      return;
+    }
+
+    this.setState({
+      selectedCurrencies: this.state.selectedCurrencies.concat(currencies[0]),
+    })
   };
 
   handleButtonClicked = () => {
-    const outputString = `name: ${this.state.name}, phoneNumber: ${this.state.phoneNumber}, currency: ${this.state.selectedCurrency}`;
+    const outputString = `name: ${this.state.name}, phoneNumber: ${this.state.phoneNumber}, selectedCurrencies: ${this.state.selectedCurrencies}`;
     alert(outputString);
   };
 
@@ -50,13 +74,22 @@ class InsertUserForm extends React.Component {
           <TextField label="Phone Number" onChange={this.handlePhoneNumberChange} />
         </div>
         <div>
-          <Select children={values} onChange={this.handleCurrencyChange} value={this.state && this.state.selectedCurrency} />
+          {
+            this.state.selectedCurrencies.map((selectedCurrency, index) => (
+              <Select children={values} onChange={(e) => { this.handleCurrencyChange(e, index)}} value={selectedCurrency} />
+            ))
+          }
         </div>
-        <div>
+        <ButtonWrapper>
+          <Button variant="contained" color="secondary" onClick={this.handleCurrencyAdded} >
+            Add new currency
+          </Button>
+        </ButtonWrapper>
+        <ButtonWrapper>
           <Button variant="contained" color="primary" onClick={this.handleButtonClicked} >
             Submit!
           </Button>
-        </div>
+        </ButtonWrapper>
       </React.Fragment>
     );
   }
