b4ebdb63f2fb3f84dc66bbc7e47f62189b912305
Daniel Hara <haradaniel734@gmail.com>
2018-12-06 16:49:24 -0200

    a welcome message

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
+const Message = () => "This is a simple app, built for fun and to exercise a little bit of React-Redux. However, it encompasses most concepts of React-Redux for beginners, like Router, store and the mapStateToProps and mapDispatchToProps functions.";
 
 const Home = () => 
   <div>
-    <Button href="/insertuser" >
-      Go to Insert Client page
-    </Button>
+    <Message />
+    <div>
+      <Button href="/insertuser" >
+        Go to Insert Client page
+      </Button>
+    </div>
   </div>;
 
 export default Home;
