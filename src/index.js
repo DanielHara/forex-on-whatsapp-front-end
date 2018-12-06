bc7bc7dc412fd4e78c69ca759eb9b5fa4bf5e0fd
Daniel Hara <haradaniel734@gmail.com>
2018-12-06 17:21:16 -0200

    delete unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
 import './index.css';
 import InsertClientPage from './InsertClientPage';
